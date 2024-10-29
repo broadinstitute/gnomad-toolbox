2bd494e65c3ed12cc3081a7b6a67c8f28b813fa5
Qin He <44242118+KoalaQin@users.noreply.github.com>
2024-10-29 12:10:20 -0400

    Modify import_data function to match gnomad_methods repo version by datatype

--- gnomad_toolbox/modules/import_data.py
+++ gnomad_toolbox/modules/import_data.py
@@ -1,7 +1,12 @@
 """Functions to import gnomAD data."""
 
 import hail as hl
+from gnomad.resources.grch37.gnomad import EXOME_RELEASES as GRCh37_EXOME_RELEASES
+from gnomad.resources.grch37.gnomad import GENOME_RELEASES as GRCh37_GENOME_RELEASES
 from gnomad.resources.grch37.gnomad import public_release as grch37_public_release
+from gnomad.resources.grch38.gnomad import EXOME_RELEASES as GRCh38_EXOME_RELEASES
+from gnomad.resources.grch38.gnomad import GENOME_RELEASES as GRCh38_GENOME_RELEASES
+from gnomad.resources.grch38.gnomad import JOINT_RELEASES as GRCh38_JOINT_RELEASES
 from gnomad.resources.grch38.gnomad import public_release as grch38_public_release
 
 
@@ -19,14 +24,34 @@ def get_ht_by_datatype_and_version(
     | genomes         | 3.0, 3.1, 3.1.1, 3.1.2, 4.0, 4.1 | 2.1, 2.1.1           |
     | joint           | 4.1                              | N/A                  |
 
-
-    :param data_type: Data type (exomes or genomes or joint).
+    :param data_type: Data type (exomes, genomes, or joint).
     :param version: gnomAD version.
     :return: Hail Table.
+    :raises ValueError: If the data type or version is invalid.
     """
-    if version in ["2.1", "2.1.1"]:
-        return grch37_public_release(data_type).ht()
-    elif version in ["3.0", "3.1", "3.1.1", "3.1.2", "4.0", "4.1"]:
+    # Mapping data types to version sets for GRCh38 and GRCh37
+    versions_by_type = {
+        "exomes": (GRCh38_EXOME_RELEASES, GRCh37_EXOME_RELEASES),
+        "genomes": (GRCh38_GENOME_RELEASES, GRCh37_GENOME_RELEASES),
+        "joint": (GRCh38_JOINT_RELEASES, []),
+    }
+
+    # Validate data type
+    if data_type not in versions_by_type:
+        raise ValueError(
+            f"Data type {data_type} is invalid. Choose from 'exomes', 'genomes', or 'joint'."
+        )
+
+    # Get GRCh38 and GRCh37 versions for the given data type
+    grch38_versions, grch37_versions = versions_by_type[data_type]
+
+    # Check version availability for GRCh38 and GRCh37
+    if version in grch38_versions:
         return grch38_public_release(data_type).ht()
+    elif version in grch37_versions:
+        return grch37_public_release(data_type).ht()
     else:
-        raise ValueError(f"Version {version} not found for data type {data_type}.")
+        raise ValueError(
+            f"Version {version} is not available for {data_type}. "
+            f"Available versions: GRCh38 - {grch38_versions}, GRCh37 - {grch37_versions}."
+        )
